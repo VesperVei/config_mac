3f5e23ea48f99ac810d6476391f9b8ef5c0aa8e5
yuzaochuan <3454731266@qq.com>
2026-03-01 13:11:50 +0800

    feat:添加了tmux的配置，并修改了setup脚本
    ui:还修改了一下tmux的UI（气泡风格｜紫）

--- nvim/lua/chenhun/core/options.lua
+++ nvim/lua/chenhun/core/options.lua
@@ -13,7 +13,7 @@ opt.autoindent = true
 -- line wrapping
 opt.wrap = false
 
--- search settings  
+-- search settings
 opt.ignorecase = true
 opt.smartcase = true
 
@@ -35,4 +35,13 @@ opt.clipboard:append("unnamedplus")
 -- split windows
 opt.splitright = true
 opt.splitbelow = true
-opt.iskeyword: append ("_")
+opt.iskeyword:append("_")
+
+-- 1. 隐藏命令行：设为 0 只有在报错或输入命令时才显现（Neovim 0.8+ 支持）
+opt.cmdheight = 0
+
+-- 2. 隐藏模式：既然 Lualine 已经有 INSERT 块了，底部的文字就是多余的
+opt.showmode = false
+
+-- 3. 确保全局状态栏开启
+opt.laststatus = 3
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 DOTFILES_DIR="$HOME/.myutils/dotfiles"
-CONFIGS=("nvim" "aerospace" "sketchybar" "kitty")
+CONFIGS=("nvim" "aerospace" "sketchybar" "kitty" "tmux")
 BACKUP_DIR="$HOME/.config/config_backup_$(date +%Y%m%d_%H%M%S)"
 
 print_menu() {
